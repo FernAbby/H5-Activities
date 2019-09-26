57a1a0ecba0330e579ef5007fd876f3c0617a3c8
wenzhen <wenzhen@szts.xyz>
2019-09-26 17:26:31 +0800

    refactor: 临时提交

--- src/pages/doll/index.js
+++ src/pages/doll/index.js
@@ -2,23 +2,29 @@ import './index.less';
 import '../../assets/js/mobile-flexible.js';
 import $ from 'expose-loader?$!jquery';
 
-$(function() {
-  const globalData = {
-    count: 0,
-  };
+const globalData = {
+  count: 5,
+  lock: false, // 抽奖锁
+};
 
+$(function() {
   const $behindBag = $(".moving-left"); // 后方运动盒子
   const $frontBag = $(".moving-right"); // 前方运动盒子
+  const $hook = $("#hook"); // 钩子
+  const $lottery = $("#lottery"); // 抽奖按钮
+  const $count = $("#count");
+  const rem = 75;
+  // parseFloat(window.document.documentElement.style.fontSize)*2
 
   let left = 0; // 后方福袋移动
   let right = 0; // 前方福袋移动
   let index = 0; //
   let timer = null;
 
-
-
-  const WIDTH = Math.floor(document.documentElement.clientWidth * 0.45); //屏幕宽度*0.45来设置li的宽度
+  const WIDTH = Math.floor(document.documentElement.clientWidth * 0.45); // 屏幕宽度*0.45来设置li的宽度
   $(".behind li,.front li").css("width", WIDTH + "px"); // 设置li的宽度
+
+
 
   const initAnimation = () => {
     window.requestAnimationFrame = window.requestAnimationFrame || function (fn) {
@@ -28,7 +34,7 @@ $(function() {
     //cancelAnimationFrame是清除requestAnimationFrame
     window.cancelAnimationFrame = window.cancelAnimationFrame || clearTimeout;
 
-    const move = () => {
+    const lineMove = () => {
       // 后方福袋的运动
       left = parseFloat($behindBag.css("margin-left")); // 获取margin-left并去掉单位
       left = left - 1.5;
@@ -37,7 +43,7 @@ $(function() {
         $behindBag.css("margin-left", "0");
       }
 
-      //前方福袋的运动
+      // 前方福袋的运动
       right = parseFloat($frontBag.css("margin-right")); // 获取margin-right并去掉单位
       right = right - 1.5;
       $frontBag.css("margin-right", right + "px");
@@ -45,12 +51,33 @@ $(function() {
         $frontBag.css("margin-right", "0");
       }
 
-      console.log(left, right);
-
-      timer = requestAnimationFrame(move);
+      timer = requestAnimationFrame(lineMove);
     };
-    move();
+    lineMove();
   };
 
   initAnimation();
+
+  $lottery.on("click", function() {
+    const { lock, count } = globalData;
+    if (lock) return;
+
+    if (count <= 0) {
+      alert("没有次数了!");
+      return;
+    }
+
+    globalData.lock = true;
+    globalData.count--;
+
+    $count.html(globalData.count);
+
+    const random = Math.floor(Math.random()*(620 - 130) + 130); // 130-620边界值
+    $(".left,.right").addClass("up"); // 张开钩子
+
+    $hook.animate({left: random / rem + "rem"}, 1000);//左右值
+
+
+
+  });
 });
